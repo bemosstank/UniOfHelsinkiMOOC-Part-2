75a86657edf85e20e8de71e86f8fc760b4652828
bemosstank <ensiah193@gmail.com>
2024-08-14 14:59:21 -0600

    completed part 8 - LiquidContainer

--- part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,17 +5,47 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        int firstContainer = 0;
+        int secondContainer = 0;
         while (true) {
-            System.out.print("> ");
-
+            System.out.println("First: " + firstContainer + "/" + 100);
+            System.out.println("Second: " + secondContainer + "/" + 100);
             String input = scan.nextLine();
             if (input.equals("quit")) {
                 break;
             }
+             String splitText[] = input.split(" ");
+            int amount = Integer.parseInt(splitText[1]);
+            if (amount < 0) {
+                continue;
+            }
+            if (splitText[0].equals("add")) {
+                firstContainer +=amount;
+                if (firstContainer > 100) {
+                    firstContainer = 100;
+                }
+            }
+            if (splitText[0].equals("move")) {
+                if (amount <= firstContainer) {
+                    firstContainer -=amount;
+                    secondContainer +=amount;
+                } else {
+                    secondContainer += firstContainer;
+                    firstContainer = 0;
 
+                }
+                if (secondContainer > 100) {
+                    secondContainer = 100;
+                }
+            }
+            if(splitText[0].equals("remove")) {
+                secondContainer = secondContainer - amount;
+                if(secondContainer < 0) {
+                    secondContainer = 0;
+                }
+            }
         }
+
     }
 
 }
--- part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -5,16 +5,31 @@ public class LiquidContainers2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-
+        Container firstContainer = new Container();
+        Container secondContainer = new Container();
         while (true) {
-            System.out.print("> ");
-
-            String input = scan.nextLine();
-            if (input.equals("quit")) {
+            System.out.println("First: " + firstContainer);
+            System.out.println("Second: " + secondContainer);
+            String userInput = scan.nextLine();
+            if (userInput.equals("quit")) {
                 break;
             }
+            String[] splitTextInput = userInput.split(" ");
+            int amount = Integer.parseInt(splitTextInput[1]);
+            if (splitTextInput[0].equals("add")) {
+                firstContainer.add(amount);
+            }
+            if (splitTextInput[0].equals("remove")) {
+                secondContainer.remove(amount);
+            }
 
+            if (splitTextInput[0].equals("move")) {
+                if (amount > firstContainer.contains()) {
+                    amount = firstContainer.contains();
+                }
+                firstContainer.remove(amount);
+                secondContainer.add(amount);
+            }
         }
     }
 
