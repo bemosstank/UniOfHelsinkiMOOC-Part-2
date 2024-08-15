332ccf1c4d8e0852619bdbebc9a155a4db9bdf98
bemosstank <ensiah193@gmail.com>
2024-08-15 14:50:45 -0600

    Completed Part 8 - Nicknames

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,13 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> nicknames = new HashMap<>();
+        nicknames.put("mattew", "matt");
+        nicknames.put("michael", "mix");
+        nicknames.put("arthur", "artie");
+        System.out.println(nicknames.get("mattew"));
+        System.out.println(nicknames.get("michael"));
+        System.out.println(nicknames.get("arthur"));
     }
 
 }
