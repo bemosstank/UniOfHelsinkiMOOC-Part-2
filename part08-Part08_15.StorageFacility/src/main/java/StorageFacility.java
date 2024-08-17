3358ccd7b1d3acd6475183198c690b7f684e8551
bemosstank <ensiah193@gmail.com>
2024-08-17 13:33:20 -0600

    Updating Part 8 exercise 14 - DictionaryOfManyTranslations

--- part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
+++ part08-Part08_15.StorageFacility/src/main/java/StorageFacility.java
@@ -8,6 +8,41 @@
  *
  * @author ensia
  */
+import java.util.HashMap;
+import java.util.ArrayList;
+
 public class StorageFacility {
+
+    private HashMap<String, ArrayList<String>> storage;
+    
+    public StorageFacility(){
+        this.storage = new HashMap<>();
+    }
+    
+    public void add(String unit, String item){
+        this.storage.putIfAbsent(unit, new ArrayList<>());
+        this.storage.get(unit).add(item);
+    }
+    
+    public ArrayList<String> contents(String storageUnit){
+        return this.storage.getOrDefault(storageUnit, new ArrayList<>());
+    }
+    
+    public void remove(String storageUnit, String item ) {
+        this.storage.get(storageUnit).remove(item);
+        if(this.storage.get(storageUnit).isEmpty()){
+           this.storage.remove(storageUnit);
+        }
+    }
+    
+    public ArrayList<String> storageUnits(){
+        ArrayList<String> storageUnits = new ArrayList<>();
+        for(String key : this.storage.keySet()){
+            if(!(this.storage.get(key).isEmpty())){
+                storageUnits.add(key);
+            }
+        }
+        return storageUnits;
+    }
     
 }
