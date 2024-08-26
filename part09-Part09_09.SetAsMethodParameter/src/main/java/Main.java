4017f67e8b4306c122de7dd75aff6fe6a4e4e8a4
bemosstank <ensiah193@gmail.com>
2024-08-25 22:53:04 -0600

    Completed Part 9 Exercise 9 - SetAsMethodParameter

--- part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
+++ part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -1,10 +1,21 @@
 
-
+import java.util.Set;
+import java.util.HashSet;
 public class Main {
 
     public static void main(String[] args) {
         // You can test your method here
+        Set<String> names = new HashSet<>();
+        names.add("first");
+        names.add("first");
+        names.add("second");
+        names.add("second");
+        names.add("second");
+        System.out.println(returnSize(names));
 
+    }
+    public static int returnSize(Set<String> names){
+        return names.size();
     }
 
     // implement the method returnSize here, which returns
