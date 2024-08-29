da30f8393c06c6048f6713d2d6627d6b8bb688ed
bemosstank <ensiah193@gmail.com>
2024-08-29 10:42:49 -0600

    Completed Part 10 Exercise 3 - PositiveNumbers

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -1,14 +1,25 @@
 
 import java.util.List;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class PositiveNumbers {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         // test your method here
-
+        List<Integer> numbers = new ArrayList<>();
+       for(int i =-5; i< 5; i++) {
+           numbers.add(i);
+       }
+        System.out.println("Postive numbers are: " + PositiveNumbers.positive(numbers));
+    }
+    
+    public static List<Integer>positive(List<Integer> numbers){
+        List<Integer> positiveNumbers = numbers.stream()
+                .filter(s -> s > 0)
+                .collect(Collectors.toList());
+        return positiveNumbers;
     }
 
 }
