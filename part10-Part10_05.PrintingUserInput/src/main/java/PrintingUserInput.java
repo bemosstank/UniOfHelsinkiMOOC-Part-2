37f96e919d27393e9a059497c2ce40d494b517cd
bemosstank <ensiah193@gmail.com>
2024-08-30 07:58:07 -0600

    Completed Part 10 Exercise 5 - PrinitingUserInput

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -6,6 +6,20 @@ public class PrintingUserInput {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        boolean trueOrFalse = true;
+        while(trueOrFalse) {
+            String userInput = scanner.nextLine();
+            if(userInput.isEmpty()) {
+                trueOrFalse = false;
+            }
+            
+            list.add(userInput);
+        }
+        
+        list.stream()
+               .map(i-> i)
+                .forEach(s-> System.out.println(s));
 
     }
 }
