db359b1b539287fdc5253705ef50de4f3639578b
bemosstank <ensiah193@gmail.com>
2024-09-10 22:32:15 -0600

    Completed Part 11 Exercise 12 - SensorsAndTemperature

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Calculator.java
@@ -3,17 +3,29 @@ package validating;
 public class Calculator {
 
     public int factorial(int num) {
-
+        if(num < 0){
+            throw new IllegalArgumentException("number should not be neagtive");
+        }
+        
         int answer = 1;
         for (int i = 1; i <= num; i++) {
+
             answer *= i;
+
         }
 
         return answer;
     }
 
     public int binomialCoefficent(int setSize, int subsetSize) {
-
+        
+        if(setSize < 0 || subsetSize < 0){
+             throw new IllegalArgumentException("SetSize should not be negative");
+        }
+        if(subsetSize > setSize){
+            throw new IllegalArgumentException("subsetSize should not greater than setSize");
+        }
+        
         int numerator = factorial(setSize);
         int denominator = factorial(subsetSize) * factorial(setSize - subsetSize);
 
